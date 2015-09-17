0f165e38cb921a0f963e7d23902d71205317abb4
Grzegorz Miejski <grzegorz.miejski@allegrogroup.com>
2015-09-17 19:32:11 +0200

    clean

--- java/src/main/java/com/getbase/hackkrk/tanks/NaiveBot.java
+++ java/src/main/java/com/getbase/hackkrk/tanks/NaiveBot.java
@@ -27,8 +27,8 @@ public class NaiveBot {
     }
 
     public void run() throws Exception {
-        TanksClient client = new TanksClient("http://10.12.202.144:9999/", "sandbox-1", "NobleGainsboroDinosaurGuanaco");
-//        TanksClient client = new TanksClient("http://10.12.202.141:9999/", "master", "NobleGainsboroDinosaurGuanaco");
+//        TanksClient client = new TanksClient("http://10.12.202.144:9999/", "sandbox-1", "NobleGainsboroDinosaurGuanaco");
+        TanksClient client = new TanksClient("http://10.12.202.141:9999/", "master", "NobleGainsboroDinosaurGuanaco");
 
         ReaderKeyboard readerKeyboard = new ReaderKeyboard();
 
